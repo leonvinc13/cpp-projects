88177bd2818b11984743c85d86435bcb02c4bae7
Leon Vincelj <leonvinc7@gmail.com>
2025-08-22 15:10:09 +0200

    core

---
+++ OOP core/Ducani.cpp
@@ -0,0 +1,149 @@
+﻿/*
+Potrebno je implementirati klasu Proizvod koja sadrži šifru i naziv proizvoda.
+Naziv proizvoda uvijek se sprema velikim slovima.
+Potrebno je implementirati get i set metode za obje varijable.
+
+Zatim je potrebno implementirati klasu Ducan koja sadrži naziv, adresu i kolekciju proizvoda.
+Klasa omogućuje dodavanje proizvoda preko metode dodajProizvod.
+Potrebno je implementirati konstruktor, destruktor, konstruktor kopije, operator dodjele kopijom te operator dodjele prijenosom.
+Kopiranje mora biti duboko, a operator prijenosa mora preuzeti vlasništvo nad resursima.
+Potrebno je implementirati i metodu ispisi koja ispisuje podatke o dućanu i njegovim proizvodima.
+
+U funkciji main demonstrirati rad svih funkcionalnosti.
+*/
+
+#include <iostream>
+#include <vector>
+#include <cctype>
+
+using namespace std;
+
+class Proizvod {
+private:
+	string sifra, naziv;
+
+public:
+	Proizvod() = default;
+
+	Proizvod(string _sifra, string _naziv) : sifra(_sifra), naziv(_naziv) {}
+
+	void setNaziv(string _naziv) {
+		for (int i = 0; i < _naziv.size(); i++) {
+			_naziv[i] = toupper(_naziv[i]);
+		}
+		naziv = _naziv;
+	}
+
+	string getNaziv() const {
+		return naziv;
+	}
+
+	void setSifra(string _sifra) {
+		sifra = _sifra;
+	}
+
+	string getSifra() const {
+		return sifra;
+	}
+};
+
+class Ducan {
+public:
+	string naziv, adresa;
+	vector<Proizvod*>proizvodi;
+
+	void dodajProizvod(string sifra, string naziv) {
+		Proizvod* p = new Proizvod();
+		p->setNaziv(naziv);
+		p->setSifra(sifra);
+		proizvodi.push_back(p);
+	}
+
+	Ducan() = default;
+
+	Ducan(string _naziv, string _adresa) : naziv(_naziv), adresa(_adresa) {}
+
+	~Ducan(){
+		for (int i = 0; i < proizvodi.size(); i++) {
+			delete proizvodi[i];
+		}
+		proizvodi.clear();
+	}
+
+	Ducan(const Ducan& d) {
+		for (int i = 0; i < d.proizvodi.size(); i++) {
+			proizvodi.push_back(new Proizvod);
+			*proizvodi[i] = *d.proizvodi[i];
+		}
+		naziv = d.naziv;
+		adresa = d.adresa;
+	}
+
+	Ducan& operator = (const Ducan& d) {
+		if (this != &d) {
+			for (int i = 0; i < proizvodi.size(); i++) {
+				delete proizvodi[i];
+			}
+			proizvodi.clear();
+			for (int i = 0; i < d.proizvodi.size(); i++) {
+				proizvodi.push_back(new Proizvod);
+				*proizvodi[i] = *d.proizvodi[i];
+			}
+			naziv = d.naziv;
+			adresa = d.adresa;
+		}
+		return *this;
+	}
+
+	Ducan& operator = (Ducan &&privremeni){
+		for (int i = 0; i < proizvodi.size(); i++) {
+			delete proizvodi[i];
+		}
+		proizvodi.clear();
+		for (int i = 0; i < privremeni.proizvodi.size(); i++) {
+			proizvodi.push_back(privremeni.proizvodi[i]);
+			privremeni.proizvodi[i] = nullptr;
+		}
+		privremeni.proizvodi.clear();
+		naziv = privremeni.naziv;
+		adresa = privremeni.adresa;
+		return *this;
+	}
+
+	void ispisi() const {
+		cout << "Ducan: " << naziv << ", " << adresa << endl;
+		for (auto p : proizvodi) {
+			cout << " - [" << p->getSifra() << "] " << p->getNaziv() << endl;
+		}
+	}
+};
+
+int main()
+{
+	Ducan d1("Konzum", "Zagreb, Ilica 1");
+	d1.dodajProizvod("101", "mlijeko");
+	d1.dodajProizvod("102", "kruh");
+
+	Ducan d2 = d1;
+	d2.dodajProizvod("103", "voda");
+
+	cout << "--- d1 ---" << endl;
+	d1.ispisi();
+	cout << "--- d2 (kopija d1 + dodan 1 proizvod) ---" << endl;
+	d2.ispisi();
+
+	Ducan d3("Plodine", "Split, Riva 10");
+	d3 = d1;
+	cout << "--- d3 (operator= duboko kopiranje d1) ---" << endl;
+	d3.ispisi();
+
+	Ducan d4("Tommy", "Osijek, Europska avenija 5");
+	d4 = Ducan("Lidl", "Rijeka, Korzo 15");
+	d4.dodajProizvod("104", "sok");
+
+	cout << "--- d4 (move operator) ---" << endl;
+	d4.ispisi();
+	return 0;
+}
+
+
---
+++ OOP core/Firma.cpp
@@ -0,0 +1,217 @@
+﻿/*
+Potrebno je implementirati klasu Zaposlenik koja sadrži ime, prezime i starost.
+Potrebno je implementirati get i set metode za sve atribute.
+Potrebno je implementirati konstruktor i destruktor.
+
+Zatim je potrebno implementirati klasu Firma koja sadrži naziv, OIB, adresu i kolekciju zaposlenika.
+Naziv firme mora sadržavati barem 5 znakova.
+OIB mora sadržavati točno 11 znamenki.
+Potrebno je implementirati više načina dodavanja zaposlenika, prijenosom vrijednosti, referencom i pokazivačem.
+Potrebno je implementirati metodu za ispis svih zaposlenika.
+
+Potrebno je implementirati metode za pronalaženje zaposlenika po imenu i prezimenu.
+Prva metoda vraća objekt po vrijednosti.
+Druga metoda vraća pokazivač na pronađenog zaposlenika.
+Treća metoda vraća referencu na pronađenog zaposlenika.
+
+U funkciji main demonstrirati rad svih funkcionalnosti.
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Zaposlenik {
+private:
+    string ime, prezime;
+    int starost;
+public:
+    void setIme(string _ime) {
+        ime = _ime;
+    }
+
+    string getIme() {
+        return ime;
+    }
+
+    void setPrezime(string _prezime) {
+        prezime = _prezime;
+    }
+
+    string getPrezime() {
+        return prezime;
+    }
+
+    void setStarost(int _starost) {
+        starost = _starost;
+    }
+
+    int getStarost() {
+        return starost;
+    }
+
+    Zaposlenik(string _ime, string _prezime, int _starost)
+        : ime(_ime), prezime(_prezime), starost(_starost) {
+        cout << "Zaposlenik constructor called" << endl;
+    }
+
+    ~Zaposlenik() {
+        cout << "Zaposlenik destructor called" << endl;
+    }
+};
+
+class Firma {
+private:
+    string naziv, oib, adresa;
+    vector<Zaposlenik> zaposlenici;
+public:
+    void setNaziv(const string &_naziv) {
+        if (_naziv.size() >= 5) {
+            naziv = _naziv;
+        }
+        else {
+            cout << "Naziv mora sadržavati barem 5 znakova." << endl;
+        }
+    }
+
+    void setOib(const string &_oib) {
+        bool ispravno = true;
+        for (int i = 0; i < _oib.size(); i++) {
+            if (!isdigit(_oib[i])) {
+                ispravno = false;
+            }
+        }
+        if (_oib.size() == 11 && ispravno == true) {
+            oib = _oib;
+        }
+        else {
+            cout << "OIB se mora sastojati od 11 znamenaka" << endl;
+        }
+    }
+
+    void setAdresa(const string& _adresa) {
+        adresa = _adresa;
+    }
+
+    string getNaziv() {
+        return naziv;
+    }
+
+    string getOib() {
+        return oib;
+    }
+
+    string getAdresa() {
+        return adresa;
+    }
+
+    void dodajZaposlenika1(Zaposlenik z);
+
+    void dodajZaposlenika2(Zaposlenik& z);
+
+    void dodajZaposlenika3(Zaposlenik* z);
+
+    void ispisSvihZaposlenika() {
+        for (int i = 0; i < zaposlenici.size(); i++) {
+            cout << i + 1 << ". " << zaposlenici[i].getIme() << " " << zaposlenici[i].getPrezime() << " " << zaposlenici[i].getStarost() << endl;
+        }
+    }
+
+    Zaposlenik nadjiZaposlenika1(string ime, string prezime);
+
+    Zaposlenik* nadjiZaposlenika2(string ime, string prezime);
+
+    Zaposlenik& nadjiZaposlenika3(string ime, string prezime);
+};
+
+
+void Firma::dodajZaposlenika1(Zaposlenik z) {
+    zaposlenici.push_back(z);
+}
+
+
+
+void Firma::dodajZaposlenika2(Zaposlenik& z) {
+    zaposlenici.push_back(z); 
+}
+
+
+void Firma::dodajZaposlenika3(Zaposlenik* z) {
+    zaposlenici.push_back(*z); 
+}
+
+
+Zaposlenik Firma::nadjiZaposlenika1(string ime, string prezime) {
+    for (int i = 0; i < zaposlenici.size(); i++) {
+        if (zaposlenici[i].getIme() == ime && zaposlenici[i].getPrezime() == prezime) {
+            return zaposlenici[i];
+        }
+    }
+    cout << "Zaposlenik nije pronađen!" << endl;
+    return Zaposlenik("", "", 0);  
+}
+
+Zaposlenik* Firma::nadjiZaposlenika2(string ime, string prezime) {
+    for (int i = 0; i < zaposlenici.size(); i++) {
+        if (zaposlenici[i].getIme() == ime && zaposlenici[i].getPrezime() == prezime) {
+            return &zaposlenici[i];  
+        }
+    }
+    return nullptr;  
+}
+
+Zaposlenik& Firma::nadjiZaposlenika3(string ime, string prezime) {
+    for (int i = 0; i < zaposlenici.size(); i++) {
+        if (zaposlenici[i].getIme() == ime && zaposlenici[i].getPrezime() == prezime) {
+            return zaposlenici[i];  
+        }
+    }
+    cout << "Zaposlenik nije pronaden!" << endl;
+    Zaposlenik prazni("","",0);  
+    return prazni;
+}
+
+
+int main()
+{
+    Firma firma;
+    firma.setNaziv("TechCorp");
+    firma.setOib("12345678901");
+    firma.setAdresa("Zagreb, Ulica 1");
+
+    Zaposlenik z1("Marko", "Markovic", 20); 
+    Zaposlenik z2("Ana", "Anic", 35); 
+    Zaposlenik z3("Ivan", "Ivic", 19); 
+
+    firma.dodajZaposlenika1(z1);  
+    firma.dodajZaposlenika2(z2);  
+    firma.dodajZaposlenika3(&z3);  
+
+    cout << "Svi zaposlenici:" << endl;
+    firma.ispisSvihZaposlenika();
+
+    Zaposlenik z4 = firma.nadjiZaposlenika1(z1.getIme(), z1.getPrezime());
+    z4.setIme("Mirko");
+    z4.setStarost(24);
+    cout << endl << "Nakon izmjene preko nadjiZaposlenika1:" << endl;
+    firma.ispisSvihZaposlenika();
+
+    Zaposlenik* z5 = firma.nadjiZaposlenika2(z2.getIme(), z2.getPrezime());
+    if (z5 != nullptr) {
+        z5->setPrezime("Kovacic");
+        z5->setStarost(65);
+    }
+    cout << endl << "Nakon izmjene preko nadjiZaposlenika2:" << endl;
+    firma.ispisSvihZaposlenika();
+
+    Zaposlenik& z6 = firma.nadjiZaposlenika3(z3.getIme(), z3.getPrezime());
+    z6.setPrezime("Ivanovic");  
+    cout << endl << "Nakon izmjene preko nadjiZaposlenika3:" << endl;
+    firma.ispisSvihZaposlenika();
+
+    return 0;
+}
+
