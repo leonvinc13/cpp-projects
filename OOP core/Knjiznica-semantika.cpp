05838dee2380151423fcfdaf8eb67b6b208f6620
Leon Vincelj <leonvinc7@gmail.com>
2025-08-22 15:14:52 +0200

    semantic

---
+++ OOP core/Knjiznica-semantika.cpp
@@ -0,0 +1,137 @@
+﻿/*
+Napiši klasu Knjiga koja sadrži privatne atribute naslov i autor. Potrebno je omogućiti postavljanje i dohvat vrijednosti, uz provjeru da naslov ima barem tri znaka.
+Napiši klasu Knjiznica koja sadrži naziv, adresu i kolekciju knjiga. Omogući dodavanje knjige, ispis podataka te pravilno upravljanje memorijom.
+Implementiraj konstruktor kopije, operator dodjele kopiranjem te operator dodjele prijenosom. Pokaži njihovo korištenje u funkciji main kroz primjere kopiranja i prijenosa.
+*/
+
+#include <iostream>
+#include <vector>
+#include <cctype>
+
+using namespace std;
+
+class Knjiga {
+private:
+    string naslov, autor;
+
+public:
+    void setNaslov(string _naslov) {
+        if (_naslov.size() >= 3) {
+            naslov = _naslov;
+        }
+        else cout << "Naslov knjige treba imati minimalno 3 znaka!";
+    }
+
+    string getNaslov() const{
+        return naslov;
+    }
+
+    void setAutor(string _autor) {
+        autor = _autor;
+    }
+
+    string getAutor() const{
+        return autor;
+    }
+
+    Knjiga() = default;
+};
+
+class Knjiznica {
+public:
+    string naziv, adresa;
+    vector <Knjiga*> knjige;
+
+    Knjiznica() = default;
+
+    ~Knjiznica() {
+        for (int i = 0; i < knjige.size(); i++) {
+            delete  knjige[i];
+        }
+        knjige.clear();
+    }
+
+    void dodajKnjigu(Knjiga knjiga) {
+        Knjiga *k = new Knjiga;
+        k->setNaslov(knjiga.getNaslov());
+        k->setAutor(knjiga.getAutor());
+        knjige.push_back(k);
+    }
+
+    Knjiznica(const Knjiznica& k) {
+        for (int i = 0; i < k.knjige.size(); i++) {
+            knjige.push_back(new Knjiga);
+            *knjige[i] = *k.knjige[i];
+        }
+        naziv = k.naziv;
+        adresa = k.adresa;
+    }
+
+    Knjiznica& operator = (const Knjiznica& k) {
+        if (this != &k) {
+            for (int i = 0; i < knjige.size(); i++) {
+                delete knjige[i];
+            }
+            knjige.clear();
+            for (int i = 0; i < k.knjige.size(); i++) {
+                knjige.push_back(new Knjiga);
+                *knjige[i] = *k.knjige[i];
+            }
+            naziv = k.naziv;
+            adresa = k.adresa;
+        }
+        return *this;
+    }
+
+    Knjiznica& operator = (Knjiznica&& privremeni) {
+        for (int i = 0; i < knjige.size(); i++) {
+            delete knjige[i];
+        }
+        knjige.clear();
+        for (int i = 0; i < privremeni.knjige.size(); i++) {
+            knjige.push_back(privremeni.knjige[i]);
+            privremeni.knjige[i] = nullptr;
+        }
+        naziv = privremeni.naziv;
+        adresa = privremeni.adresa;
+        return *this;
+    }
+
+    void ispisi() const {
+        cout << "Knjiznica: " << naziv << ", " << adresa << endl;
+        for (auto k : knjige) {
+            cout << " - \"" << k->getNaslov() << "\" od " << k->getAutor() << endl;
+        }
+    }
+};
+
+int main()
+{
+    Knjiznica k1;
+    k1.naziv = "Opera";
+    k1.adresa = "Zagreb 1";
+    Knjiga knjiga1;
+    knjiga1.setNaslov("Priče");
+    knjiga1.setAutor("Ivica Ivić");
+    k1.dodajKnjigu(knjiga1);
+
+    Knjiznica k2 = k1;
+
+    Knjiznica k3;
+    k3 = k2;
+
+    Knjiznica k4;
+    k4 = Knjiznica();
+
+    cout << "--- k1 ---" << endl;
+    k1.ispisi();
+    cout << "--- k2 (kopija k1) ---" << endl;
+    k2.ispisi();
+    cout << "--- k3 (dodijeljena kopija k2) ---" << endl;
+    k3.ispisi();
+    cout << "--- k4 (move dodjela praznog objekta) ---" << endl;
+    k4.ispisi();
+    return 0;
+}
+
+
---
+++ OOP core/Kolegiji-kopiranje.cpp
@@ -0,0 +1,100 @@
+﻿/*
+Napraviti klasu Student s atributima ime, jmbag i broj bodova.
+Napraviti klasu Kolegij koja sadrži listu studenata.
+Implementirati dodavanje studenta u kolegij.
+Implementirati konstruktor kopije s dubokim kopiranjem.
+Implementirati operator dodjele s dubokim kopiranjem.
+Implementirati operator dodjele s prijenosom.
+Testirati sve funkcionalnosti u glavnom programu.
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Student {
+public:
+	string ime, jmbag;
+	int brojBodova = 0;
+	Student() = default;
+	Student(string _ime, string _jmbag) : ime(_ime), jmbag(_jmbag) {}
+};
+
+class Kolegij {
+public:
+	vector<Student*>student;
+	
+	void dodaj(Student* s) {
+		student.push_back(s);
+	}
+
+	Kolegij() = default;
+
+	~Kolegij() {
+		for (int i = 0; i < student.size(); i++) {
+			delete student[i];
+		}
+	}
+
+	Kolegij(const Kolegij &k) {
+		for (int i = 0; i < k.student.size(); i++) {
+			//student.push_back(new Student(k.student[i]->ime, k.student[i]->jmbag));
+			student.push_back(new Student);
+			*student[i] = *k.student[i];
+			student[i]->brojBodova = k.student[i]->brojBodova;
+		}
+	}
+
+	Kolegij& operator = (const Kolegij& k) {
+		if (this != &k) {
+			for (int i = 0; i < student.size(); i++) {
+				delete student[i];
+			}
+			student.clear();
+			for (int i = 0; i < k.student.size(); i++) {
+				student.push_back(new Student(k.student[i]->ime, k.student[i]->jmbag));
+				student[i]->brojBodova = k.student[i]->brojBodova;
+			}
+		}
+		return* this;
+	}
+
+	Kolegij& operator = (Kolegij &&privremeni){
+		for (int i = 0; i < student.size(); i++) {
+			delete student[i];
+		}
+		student.clear();
+		for (int i = 0; i < privremeni.student.size(); i++) {
+			student.push_back(privremeni.student[i]);
+			privremeni.student[i] = NULL;
+		}
+		return *this;
+	}
+};
+
+int main() {
+	Kolegij OOP;
+	OOP.dodaj(new Student("Ante Antic", "02460123456"));
+	OOP.dodaj(new Student("Ivica Ivic", "02460567890"));
+
+	Kolegij OOP2 = OOP;
+	OOP.student[0]->brojBodova = 10;
+	OOP2.student[0]->brojBodova = 20;
+	cout << OOP.student[0]->brojBodova << endl; // 10
+	cout << OOP2.student[0]->brojBodova << endl; // 20
+
+	Kolegij Java;
+	Java = OOP2;
+	OOP2.student[0]->brojBodova = 10;
+	Java.student[0]->brojBodova = 20;
+	cout << OOP2.student[0]->brojBodova << endl; // 10
+	cout << Java.student[0]->brojBodova << endl; // 20
+
+	Kolegij Matematika;
+	Matematika = Kolegij();
+	return 0;
+}
+
+
+
