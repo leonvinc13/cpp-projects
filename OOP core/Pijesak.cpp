afd15b549208433590534e6056ab56a3c445f0d2
Leon Vincelj <leonvinc7@gmail.com>
2025-08-22 15:18:37 +0200

    copy

---
+++ OOP core/Pijesak.cpp
@@ -0,0 +1,97 @@
+﻿/*
+Napraviti klasu Zrno koja sadrži boju i promjer.
+Napraviti klasu Pijesak koja ima količinu i polje pokazivača na objekte tipa Zrno.
+U konstruktoru klase Pijesak dinamički alocirati polje zrna.
+U destruktoru osloboditi zauzetu memoriju.
+Definirati kopirni konstruktor koji radi duboku kopiju.
+Definirati operator pridruživanja (kopiranje) koji radi duboku kopiju.
+Definirati konstruktor prijenosa koji preuzima podatke iz privremenog objekta.
+Definirati operator pridruživanja (prijenos) koji preuzima podatke iz privremenog objekta.
+U funkciji main demonstrirati rad svih konstruktora i operatora pridruživanja.
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Zrno {
+public:
+    string boja;
+    float promjer;
+};
+class Pijesak {
+public:
+    int kolicina;
+    Zrno* zrno;
+    Pijesak() = default;
+    Pijesak(int _kolicina) : kolicina(_kolicina) {
+        // dinamički alocirajte polje 'zrno'
+        zrno = new Zrno[kolicina];
+    }
+    ~Pijesak() {
+        // ?
+        delete[] zrno;
+    }
+
+    Pijesak(const Pijesak &p) {
+        zrno = new Zrno[p.kolicina];
+        for (int i = 0; i < p.kolicina; i++) {
+            zrno[i].boja = p.zrno[i].boja;
+            zrno[i].promjer = p.zrno[i].promjer;
+        }
+        kolicina = p.kolicina;
+        cout << "kopirni konstruktor\n";
+    }
+
+    Pijesak& operator = (const Pijesak& p) {
+        if (this != &p) {
+            delete[] zrno;
+            zrno = new Zrno[p.kolicina];
+            for (int i = 0; i < p.kolicina; i++) {
+                zrno[i].boja = p.zrno[i].boja;
+                zrno[i].promjer = p.zrno[i].promjer;
+            }
+            kolicina = p.kolicina;
+            cout << "operator pridruzivanja (kopiranje)\n";
+        }
+        return *this;
+    }
+
+    Pijesak(Pijesak&& privremeni) {
+        zrno = privremeni.zrno;
+        privremeni.zrno = NULL;
+        kolicina = privremeni.kolicina;
+        cout << "konstruktor prijenosa\n";
+    }
+
+    Pijesak& operator = (Pijesak&& privremeni) {
+        if (this != &privremeni) {
+            delete[] zrno;
+            zrno = privremeni.zrno;
+            privremeni.zrno = NULL;
+            kolicina = privremeni.kolicina;
+            cout << "operator pridruzivanja (prijenos)\n";
+        }
+        return *this;
+    }
+};
+int main() {
+    Pijesak igraliste(1000000);
+    cout << "Na igralistu ima " << igraliste.kolicina << " zrna pijeska!\n";
+
+    // Demonstrirajte pozive oba konstruktora i oba operatora pridruživanja
+    Pijesak p1 = igraliste; // kopirni konstruktor
+    cout << "Na igralistu ima " << p1.kolicina << " zrna pijeska!\n";
+
+    Pijesak p2(0);
+    p2 = p1;  // operator pridruzivanja (kopiranje)
+    cout << "Na igralistu ima " << p2.kolicina << " zrna pijeska!\n";
+
+    Pijesak p3 = move(Pijesak(500000)); // konstruktor prijenosa
+    cout << "Na igralistu ima " << p3.kolicina << " zrna pijeska!\n";
+
+    Pijesak p4(0);
+    p4 = Pijesak(500000); // operator pridruzivanja (prijenos)
+    cout << "Na igralistu ima " << p4.kolicina << " zrna pijeska!\n";
+    return 0;
+}
---
+++ OOP core/Ucionica_racunalo-semantika.cpp
@@ -0,0 +1,99 @@
+﻿/*
+Napraviti klasu Racunalo koja sadrži naziv, procesor i memoriju.
+Napraviti klasu Ucionica koja sadrži broj računala i polje pokazivača na objekte tipa Racunalo.
+U konstruktoru klase Ucionica dinamički alocirati polje računala i inicijalizirati sva računala istim vrijednostima.
+U destruktoru osloboditi memoriju.
+Definirati kopirni konstruktor koji radi duboku kopiju.
+Definirati operator pridruživanja (kopiranje) koji radi duboku kopiju.
+Definirati konstruktor prijenosa koji preuzima polje iz privremenog objekta.
+Definirati operator pridruživanja (prijenos) koji preuzima polje iz privremenog objekta.
+U funkciji main demonstrirati rad svih konstruktora i operatora pridruživanja.
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Racunalo {
+public:
+    string naziv, procesor, memorija;
+};
+
+class Ucionica {
+public:
+	int brRacunala = 0;
+    Racunalo *racunalo;
+
+	~Ucionica(){
+		delete[] racunalo;
+	}
+
+	Ucionica(int _broj_racunala, string _naziv, string _procesor, string _memorija) {
+		brRacunala = _broj_racunala;
+		racunalo = new Racunalo[brRacunala];
+		for (int i = 0; i < brRacunala; i++) {
+			racunalo[i].naziv = _naziv;
+			racunalo[i].procesor = _procesor;
+			racunalo[i].memorija = _memorija;
+		}
+	}
+
+	Ucionica(const Ucionica& u) {
+		brRacunala = u.brRacunala;
+		racunalo = new Racunalo[u.brRacunala];
+		for (int i = 0; i < u.brRacunala; i++) {
+			racunalo[i].naziv = u.racunalo[i].naziv;
+			racunalo[i].procesor = u.racunalo[i].procesor;
+			racunalo[i].memorija = u.racunalo[i].memorija;
+		}
+	}
+
+	Ucionica& operator = (const Ucionica& u) {
+		if (this != &u) {
+			delete[] racunalo;
+			brRacunala = u.brRacunala;
+			racunalo = new Racunalo[u.brRacunala];
+			for (int i = 0; i < u.brRacunala; i++) {
+				racunalo[i].naziv = u.racunalo[i].naziv;
+				racunalo[i].procesor = u.racunalo[i].procesor;
+				racunalo[i].memorija = u.racunalo[i].memorija;
+			}
+		}
+		return *this;
+	}
+
+	Ucionica(Ucionica&& privremeni) {
+		brRacunala = privremeni.brRacunala;
+		racunalo = privremeni.racunalo;
+		privremeni.racunalo = NULL;
+		privremeni.brRacunala = 0;
+	}
+
+	Ucionica& operator =(Ucionica&& privremeni) {
+		delete[] racunalo;
+		racunalo = privremeni.racunalo;
+		brRacunala = privremeni.brRacunala;
+		privremeni.racunalo = NULL;
+		privremeni.brRacunala = 0;
+		return *this;
+	}
+};
+
+int main()
+{
+	// Učionica 'A' s 15 računala istih specifikacija
+	Ucionica A(15, "Koncar", "Intel I5", "4 GB");
+	// Učionica B ima također 15 računala istih specifikacija kao i učionica 'A'
+	Ucionica B = A;
+	cout << B.brRacunala << endl; // 15
+	// Računala iz učionice 'C' se prenose u  učionicu 'B'
+	Ucionica C = move(B);
+	cout << B.brRacunala << endl; // 0
+	// Privremena ucionica se zatvara i njena racunala se prenose u ucionicu 'B'
+	B = Ucionica(15, "Dell", "Intel I7", "8 GB");
+	return 0;
+
+}
+
+
